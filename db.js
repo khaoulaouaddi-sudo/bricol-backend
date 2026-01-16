4243ab687eeeac2df4627349fb6b91cc99d58635
khaoulaouaddi-sudo <khaoula.ouaddi@gmail.com>
2026-01-16 19:53:54 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,25 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // ✅ Production (Render)
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  // ✅ Local (dev)
+  pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  });
+}
 
 module.exports = pool;
