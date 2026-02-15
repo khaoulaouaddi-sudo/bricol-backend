6267ef82d0e0c9aa1856a6fcf638637b41e2bd27
Khaoula <khaoula.ouaddi@gmail.com>
2026-02-15 14:31:18 +0000

    modofier les restrcition sur le mot de passe

--- utils/passwordPolicy.js
+++ utils/passwordPolicy.js
@@ -1,19 +1,10 @@
 // utils/passwordPolicy.js
 function validatePasswordStrength(password) {
   const p = String(password || "");
-
   const issues = [];
-  if (p.length < 10) issues.push("Le mot de passe doit contenir au moins 10 caractères.");
-  if (!/[a-z]/.test(p)) issues.push("Ajoute au moins une lettre minuscule.");
-  if (!/[A-Z]/.test(p)) issues.push("Ajoute au moins une lettre majuscule.");
-  if (!/\d/.test(p)) issues.push("Ajoute au moins un chiffre.");
-  if (!/[^\w\s]/.test(p)) issues.push("Ajoute au moins un caractère spécial (ex: ! @ # ?).");
-  if (/\s/.test(p)) issues.push("Évite les espaces dans le mot de passe.");
 
-  // Empêche les mots de passe trop simples
-  const common = ["password", "123456", "123456789", "azerty", "qwerty"];
-  if (common.some((c) => p.toLowerCase().includes(c))) {
-    issues.push("Évite un mot de passe trop commun.");
+  if (p.length < 4) {
+    issues.push("Le mot de passe doit contenir au moins 4 caractères.");
   }
 
   return { ok: issues.length === 0, issues };
